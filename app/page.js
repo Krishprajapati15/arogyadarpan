e36f54970580b07af699fe7073be6110ceb3c41d
Krishprajapati15 <prajapatikrish132005@gmail.com>
2025-11-05 21:56:35 +0530

    remove commments

--- app/page.js
+++ app/page.js
@@ -136,7 +136,6 @@ export default function Home() {
 
       <section id="pricing" className="py-10 md:py-20">
         {" "}
-        {/* Added md:py-20 */}
         <div className="container mx-auto px-4">
           <div className="text-center mb-10">
             <ContainerTextFlip
